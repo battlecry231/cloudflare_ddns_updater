08e2802f11e898951378417236732d8639b14173
Phil Osterhold <38531891+battlecry231@users.noreply.github.com>
2024-05-31 19:04:13 +0200

    refactoring

--- ddns.py
+++ ddns.py
@@ -21,28 +21,28 @@ client = Cloudflare(
 
 try:
     # requesting old ip
-    old_ip = client.dns.records.get(dns_record_id=DNS_RECORD_ID, zone_id=ZONE_ID)
+    old_ip_response = client.dns.records.get(dns_record_id=DNS_RECORD_ID, zone_id=ZONE_ID)
 
     # requesting new ip
-    if old_ip.type == 'A':
+    if old_ip_response.type == 'A':
         new_ip_response = requests.get('https://api.ipify.org', timeout=REQUEST_TIMEOUT_S)
-    elif old_ip.type == 'AAAA':
+    elif old_ip_response.type == 'AAAA':
         new_ip_response = requests.get('https://api64.ipify.org', timeout=REQUEST_TIMEOUT_S)
     else:
-        sys.stderr.write(f"IP type {old_ip.type} unknown\n")
+        sys.stderr.write(f"IP type {old_ip_response.type} unknown\n")
         sys.exit(1)
 
     new_ip = new_ip_response.text
 
     # check if the old ip is different to the new ip and edit the dns record accordingly
-    if old_ip != new_ip:
-        print(f"setting new IP: {old_ip} --> {new_ip}")
+    if old_ip_response.content != new_ip:
+        print(f"setting new IP: {old_ip_response.content} --> {new_ip}")
         client.dns.records.edit(
             dns_record_id=DNS_RECORD_ID,
             zone_id=ZONE_ID,
             content=new_ip,
-            name=old_ip.name,
-            type=old_ip.type)
+            name=old_ip_response.name,
+            type=old_ip_response.type)
     else:
         print("IP not changed")
 
